d389f9a4d038dd729c269b28892eff67eeb531b3
hwnsng <amc214677@gmail.com>
2026-09-18 11:05:48 +0900

    fix: 회원가입 비밀번호 조합과 길이 검증 강화

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer';
-import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 import { OptionalField } from '../../common/validation/optional-field.decorator';
 
 export class LoginDto {
@@ -11,12 +11,20 @@ export class LoginDto {
   email!: string;
 
   @IsString()
-  @MinLength(12)
+  @MinLength(8)
   @MaxLength(128)
   password!: string;
 }
 
 export class RegisterDto extends LoginDto {
+  @IsString()
+  @MinLength(8)
+  @MaxLength(128)
+  @Matches(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\x21-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]).+$/, {
+    message: 'Password must include an English letter, a number and a symbol',
+  })
+  declare password: string;
+
   @OptionalField()
   @IsString()
   @MinLength(1)
